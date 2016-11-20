f14a0786cc1de52c0efb2edc32f79d757c7d99ec
Marc Guyer <marcguyer@gmail.com>
2016-11-20 17:38:17 -0500

    Use data attribute for share url

--- static/js/app/facebook.js
+++ static/js/app/facebook.js
@@ -1,29 +1,34 @@
-function Facebook() {
+function Facebook(element) {
 
-	window.fbAsyncInit = function() {
-	  FB.init({
-	    appId      : '1810257965898767',
-	    xfbml      : true,
-	    version    : 'v2.8'
-	  });
-	};
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '1810257965898767',
+            xfbml: true,
+            version: 'v2.8'
+        });
+    };
 
-	(function(d, s, id){
-	   var js, fjs = d.getElementsByTagName(s)[0];
-	   if (d.getElementById(id)) {return;}
-	   js = d.createElement(s); js.id = id;
-	   js.src = "//connect.facebook.net/en_US/sdk.js";
-	   fjs.parentNode.insertBefore(js, fjs);
-	 }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-	  document.getElementById('fb_share_button').onclick = function() {
-	    FB.ui({
-	      method: 'share',
-	      display: 'popup',
-	      href: 'http://bppwrite.github.io/',
-	    }, function(response){});
-	  }
+    element.onclick = function(e) {
+        FB.ui({
+            method: 'share',
+            display: 'popup',
+            href: e.target.dataset.permalink,
+        }, function(response) {});
+    }
 
 }
 
-export { Facebook };
+export {
+    Facebook
+};
