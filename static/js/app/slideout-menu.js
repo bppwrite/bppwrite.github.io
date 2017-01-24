0beeb26d406e2a6fedb0e11cb2868e6cdf080a66
Matt Batman <matt@cheddargetter.com>
2017-01-24 11:09:08 -0500

    Hid full nav on page load and switch control once slideout loads, switched JS to spaces

--- static/js/app/slideout-menu.js
+++ static/js/app/slideout-menu.js
@@ -1,45 +1,49 @@
 import Slideout from 'slideout';
 import { Observable } from 'rxjs/Observable';
 import './rxjs-operators';
+import { V } from './jVanilla';
 
 function SlideoutMenu() {
-	let slideoutConfig;
+  let slideoutConfig;
+  let dollar = V();
 
-	function init() {
-		slideoutConfig = new Slideout({
-			'panel': document.getElementById('panel'),
-			'menu': document.getElementById('menu'),
-			'padding': 256,
-			'side': 'right',
-			'fx': 'ease-in',
-			'duration': 400,
-			'tolerance': 70
-		});
+  function init() {
+    slideoutConfig = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('menu'),
+      'padding': 256,
+      'side': 'right',
+      'fx': 'ease-in',
+      'duration': 400,
+      'tolerance': 70
+    });
 
-		document.querySelector('.toggle-button').addEventListener('click', function() {
-			slideoutConfig.toggle();
-		});
-	}
+    dollar.removeClass(document.querySelector('nav#menu'), 'init-hide-menu');
 
-	function hashFix() {
-		let panel = document.getElementById('panel');
-		let scroll$ = Observable.fromEvent(window, 'scroll')
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+      slideoutConfig.toggle();
+    });
+  }
+
+  function hashFix() {
+    let panel = document.getElementById('panel');
+    let scroll$ = Observable.fromEvent(window, 'scroll')
 			.filter((event) => { return slideoutConfig.isOpen(); });
-		let scrollObserver = {
-			next: (x) => {
-				slideoutConfig.close();
-			},
-			error: (e) => { console.log(e); },
-			complete: () => {}
-		};
+    let scrollObserver = {
+      next: (x) => {
+        slideoutConfig.close();
+      },
+      error: (e) => { console.log(e); },
+      complete: () => {}
+    };
 
-		scroll$.subscribe(scrollObserver);
-	}
+    scroll$.subscribe(scrollObserver);
+  }
 
-	return {
-		init: init,
-		hashFix: hashFix
-	};
+  return {
+    init: init,
+    hashFix: hashFix
+  };
 }
 
 export { SlideoutMenu };
